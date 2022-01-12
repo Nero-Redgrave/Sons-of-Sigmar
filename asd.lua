b3d619a8d16dfb73a439093eb94a35a9d2ea1a91
Nero-Redgrave <97378166+Nero-Redgrave@users.noreply.github.com>
2022-01-12 16:08:39 -0500

    Add files via upload

---
+++ asd.lua
@@ -0,0 +1,269 @@
+local mod = get_mod("asd")
+
+mod:echo("Reiklander's Never Cry v4.0")
+--////////////////////////////////////////////////////////////////////////Kruber////////////////////////////////////////////////////////////////////////////
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"es_longbow") then
+			weapon_data.can_wield = {
+					"es_huntsman",
+					"es_knight",
+					"es_mercenary"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"es_2h_heavy_spear") then
+			weapon_data.can_wield = {
+					"es_huntsman",
+					"es_knight",
+					"es_mercenary",
+					"es_questingknight"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"es_halberd") then
+			weapon_data.can_wield = {
+					"es_huntsman",
+					"es_knight",
+					"es_mercenary",
+					"es_questingknight"
+					}
+				end
+			end			
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"es_deus_01") then
+			weapon_data.can_wield = {
+					"es_huntsman",
+					"es_knight",
+					"es_mercenary",
+					"es_questingknight"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"es_sword_shield_breton") then
+			weapon_data.can_wield = {
+					"es_huntsman",
+					"es_knight",
+					"es_mercenary",
+					"es_questingknight"
+					}
+				end
+			end
+--///////////////////////////////////////////////////////////////////////Bardin/////////////////////////////////////////////////////////////////////////////
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"dr_dual_wield_axes") then
+			weapon_data.can_wield = {
+					"dr_slayer",
+					"dr_ironbreaker",
+					"dr_ranger",
+					"dr_engineer"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"dr_drakegun") then
+			weapon_data.can_wield = {
+					"dr_ironbreaker",
+					"dr_ranger",
+					"dr_engineer"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"dr_crossbow") then
+			weapon_data.can_wield = {
+					"dr_ironbreaker",
+					"dr_ranger",
+					"dr_engineer"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"dr_1h_throwing_axes") then
+			weapon_data.can_wield = {
+					"dr_slayer",
+					"dr_ironbreaker",
+					"dr_ranger",
+					"dr_engineer"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"dr_shield_axe") then
+			weapon_data.can_wield = {
+					"dr_slayer",
+					"dr_ironbreaker",
+					"dr_ranger",
+					"dr_engineer"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"dr_shield_hammer") then
+			weapon_data.can_wield = {
+					"dr_slayer",
+					"dr_ironbreaker",
+					"dr_ranger",
+					"dr_engineer"
+					}
+				end
+			end
+--//////////////////////////////////////////////////////////////////////Kerrillian//////////////////////////////////////////////////////////////////////////
+--Staff on WS/HM/Shade *
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"we_crossbow_repeater") then
+			weapon_data.can_wield = {
+					"we_shade",
+					"we_maidenguard",
+					"we_waywatcher",
+					"we_thornsister"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"we_1h_spears_shield") then
+			weapon_data.can_wield = {
+					"we_shade",
+					"we_maidenguard",
+					"we_waywatcher",
+					"we_thornsister"
+					}
+				end
+			end
+--///////////////////////////////////////////////////////////////////////Saltzpyre//////////////////////////////////////////////////////////////////////////
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"wh_fencing_sword") then
+			weapon_data.can_wield = {
+					"wh_bountyhunter",
+					"wh_captain",
+					"wh_zealot",
+					"wh_priest"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"wh_1h_falchion") then
+			weapon_data.can_wield = {
+					"wh_bountyhunter",
+					"wh_captain",
+					"wh_zealot",
+					"wh_priest"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"wh_2h_sword") then
+			weapon_data.can_wield = {
+					"wh_bountyhunter",
+					"wh_captain",
+					"wh_zealot",
+					"wh_priest"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"wh_1h_axe") then
+			weapon_data.can_wield = {
+					"wh_bountyhunter",
+					"wh_captain",
+					"wh_zealot",
+					"wh_priest"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"es_1h_flail") then
+			weapon_data.can_wield = {
+					"wh_bountyhunter",
+					"wh_captain",
+					"wh_zealot",
+					"wh_priest"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"wh_2h_billhook") then
+			weapon_data.can_wield = {
+					"wh_bountyhunter",
+					"wh_captain",
+					"wh_zealot",
+					"wh_priest"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"wh_dual_wield_axe_falchion") then
+			weapon_data.can_wield = {
+					"wh_bountyhunter",
+					"wh_captain",
+					"wh_zealot",
+					"wh_priest"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"wh_1h_hammer") then
+			weapon_data.can_wield = {
+					"wh_bountyhunter",
+					"wh_captain",
+					"wh_zealot",
+					"wh_priest"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"wh_2h_hammer") then
+			weapon_data.can_wield = {
+					"wh_bountyhunter",
+					"wh_captain",
+					"wh_zealot",
+					"wh_priest"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"wh_dual_hammer") then
+			weapon_data.can_wield = {
+					"wh_bountyhunter",
+					"wh_captain",
+					"wh_zealot",
+					"wh_priest"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"wh_hammer_shield") then
+			weapon_data.can_wield = {
+					"wh_bountyhunter",
+					"wh_captain",
+					"wh_zealot",
+					"wh_priest"
+					}
+				end
+			end
+for _, weapon_data in pairs( ItemMasterList ) do
+		if string.find (_,"wh_flail_shield") then
+			weapon_data.can_wield = {
+					"wh_bountyhunter",
+					"wh_captain",
+					"wh_zealot",
+					"wh_priest"
+					}
+				end
+			end
+
+--BnH on WHC/BH/Zealot *
+
+
+--Crashing on Drakefire pistols RV
+--for _, weapon_data in pairs( ItemMasterList ) do
+--		if string.find (_,"dr_drake_pistol") then
+--			weapon_data.can_wield = {
+--					"dr_ironbreaker",
+--					"dr_ranger",
+--					"dr_engineer"
+--					}
+--				end
+--			end
